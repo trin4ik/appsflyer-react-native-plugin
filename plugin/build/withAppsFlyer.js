285a3297138c4c35060c6b5bdf157f1098d74324
Amit7474 <amit741993@gmail.com>
2021-06-03 12:54:33 +0300

    expo support

---
+++ plugin/build/withAppsFlyer.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config_plugins_1 = require("@expo/config-plugins");
+const pkg = require('react-native-appsflyer/package.json');
+const CAMERA_USAGE = 'Allow $(PRODUCT_NAME) to access your camera';
+const MICROPHONE_USAGE = 'Allow $(PRODUCT_NAME) to access your microphone';
+
+const withAppsFlyer = (config, { cameraPermission, microphonePermission } = {}) => {
+    if (!config.ios)
+        config.ios = {};
+    if (!config.ios.infoPlist)
+        config.ios.infoPlist = {};
+    config.ios.infoPlist.NSCameraUsageDescription =
+        cameraPermission || config.ios.infoPlist.NSCameraUsageDescription || CAMERA_USAGE;
+    config.ios.infoPlist.NSMicrophoneUsageDescription =
+        microphonePermission || config.ios.infoPlist.NSMicrophoneUsageDescription || MICROPHONE_USAGE;
+    return config_plugins_1.AndroidConfig.Permissions.withPermissions(config, ['android.permission.CAMERA']);
+};
+exports.default = config_plugins_1.createRunOncePlugin(withAppsFlyer, pkg.name, pkg.version);
